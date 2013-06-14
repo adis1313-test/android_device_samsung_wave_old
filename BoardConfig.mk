9ce4327b7682d1ca3e9862dcd426ce714d1db120
adis1313 <adis1313@ubuntu.(none)>
2013-06-14 17:45:00 +0200

    Sources adjusted to 4.2.2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,9 +36,11 @@ BOARD_NO_PAGE_FLIPPING := false
 BOARD_NO_32BPP := false
 
 
+TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a8
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_NO_BOOTLOADER := true
@@ -107,6 +109,10 @@ WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 
 # adb has root
 ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/wave/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/wave/prebuilt/libbt_vndcfg.txt
 
 # Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
